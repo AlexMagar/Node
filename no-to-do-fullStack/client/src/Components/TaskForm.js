6176cee9e17e50ea6f164d628ceda06c4271078b
AlexMagar <lx.mgr07@gmail.com>
2023-06-09 22:15:19 +1000

    added FlotingLabel container, and aria-describedy to Form.Control type number container

--- no-to-do-fullStack/client/src/Components/TaskForm.js
+++ no-to-do-fullStack/client/src/Components/TaskForm.js
@@ -1,4 +1,4 @@
-import { Button } from 'react-bootstrap';
+import { Button, FloatingLabel } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
@@ -8,10 +8,13 @@ export const TaskForm = () => {
     <Form className=" border p-2 bg-light rounded">
       <Row className="g-2">
         <Col md="6">
-          <Form.Control required placeholder="watching tv" name="task" />
+          <FloatingLabel controlId='floatingInput' className='mb-3'>
+          <Form.Control required placeholder="Add Task" name="task" />
+          </FloatingLabel>
         </Col>
         <Col md="2">
-          <Form.Control required placeholder="33" type="number" />
+          <Form.Control required placeholder="3" type="number" aria-describedby='hoursHelpBlock'/>
+          <Form.Text id="hoursHelpBlock" muted> Type how many hours you want to allocated for the task </Form.Text>
         </Col>
         <Col className="d-grid">
           <Button variant="primary">Add Task</Button>
