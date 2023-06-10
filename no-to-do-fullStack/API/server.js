2936733968160dc7deed827c2f30a036d9048e31
AlexMagar <lx.mgr07@gmail.com>
2023-06-10 21:48:55 +1000

    mongodb connection

--- no-to-do-fullStack/API/server.js
+++ no-to-do-fullStack/API/server.js
@@ -1,9 +1,13 @@
 import express from "express"
 import taskRouter from "./src/routers/taskRouter.js"
+import { mongoConnect } from "./src/config/mongoDb.js";
 
 
 const app = express();
 const PORT = 8000;
+// connect mongoDb
+
+mongoConnect();
 
 //middleware
 app.use(express.json());
