cef7cdb24eb37acaaaad16251ae456e44f5df238
AlexMagar <lx.mgr07@gmail.com>
2023-06-11 20:56:06 +1000

    added new Components, TaskContainer and ToastConatine

--- no-to-do-fullStack/client/src/App.js
+++ no-to-do-fullStack/client/src/App.js
@@ -2,52 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import { TaskForm } from './Components/TaskForm';
 import { Col, Container, Row, Table } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { fetchTasks } from './helper/axiosHelper';
+import { useState } from 'react';
+import { TaskContainer } from './Components/TaskContainer';
 
 function App() {
+
+  const [list, setlist] = useState([]);
+
+  const getTaskList = async () =>{
+    const {status, taskList} = await fetchTasks();
+
+    if(status === "sucess" && taskList.length){
+      setlist(taskList);
+    }
+  }
+
   return (
-    <div className="wrapper">
+    <div className='wrapper'>
       <Container>
         <Row>
           <Col className='mt-5 text-center fs-1 mb-2'>Task Mgmt</Col>
         </Row>
         {/* from */}
-        <TaskForm/>
+        <TaskForm getTaskList={getTaskList}/>
 
         {/* task display table */}
-        <Row className='mt-5'>
-          <Col>
-          <h3 className='text-center'>Entry List</h3>
-          <hr/>
-          <Table striped bordered hover className='bg-transparent'>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>Thornton</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </Table>
-          </Col>
-          <Col>
-          <h3 className='text-center'>Bad List</h3>
-          <hr/>
-          </Col>
-        </Row>
+        <TaskContainer list={list}/>
+
       </Container>
-    </div>
+      <ToastContainer/>
+      </div>
   );
 }
 
