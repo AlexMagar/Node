7f08e665b6e390f0e5a02060d9d7c1ee2457ecb9
AlexMagar <lx.mgr07@gmail.com>
2023-06-11 20:58:16 +1000

    made List container that contain list of the task that user add

---
+++ no-to-do-fullStack/client/src/Components/TaskContainer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Col, Row, Table } from 'react-bootstrap';
+
+export const TaskContainer = ({list}) => {
+  return (
+    <div>
+        <Row className='mt-5'>
+          <Col>
+          <h3 className='text-center'>Entry List</h3>
+          <hr/>
+          <Table striped bordered hover className='bg-none'>
+            <tbody>
+                {list.map((item, i) =>(
+                <tr key={i}>
+                    <td>{i + 1}</td>
+                    <td>{item.task}</td>
+                    <td>{item.hr}</td>
+                    <td>buttons</td>
+                </tr>
+                ))}
+            </tbody>
+          </Table>
+          </Col>
+          <Col>
+          <h3 className='text-center'>Bad List</h3>
+          <hr/>
+          </Col>
+        </Row>
+    </div>
+  )
+}
